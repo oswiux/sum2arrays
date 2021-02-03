678f131f4bb797f322385816df059314a3881776
oswiux <35800600+oswiux@users.noreply.github.com>
2021-02-03 17:43:22 +0200

    array + array

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-// function arrayPlusArray(arr1, arr2) {
-//     let sum1 = 0;
-//     for (let i = 0, i < arr1.lenght, i++);
-//     sum1 += arr1[i];
-//     let sum2 = 0;
-//     for (let i = 0, i < arr1.lenght, i++);
-//     sum1 += arr2[i];
-    
-//     return arr1 + arr2; //something went wrong
-//   }
 
 
+    function arrayPlusArray(arr1, arr2) {
+        sum1 = 0;
+        for(let i=0;i<arr1.length;i++) {
+            sum1 += arr1[i]
+        }
+        sum2 = 0
+        for(let i=0;i<arr2.length;i++) {
+            sum2 += arr2[i]
+        }
+        return sum1 + sum2;
+    }
 
-//     console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+    console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
 
-
-function(sumarray) {
-    
-}
-
-
-    console.log([1, 2, 3]);
+// trumpesnis pavyzdys
+    const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((a, b) => a + b, 0);
