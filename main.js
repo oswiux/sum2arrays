28647dfb8a9ed9f237c9522b81f848f4974e9cdc
oswiux <35800600+oswiux@users.noreply.github.com>
2021-02-03 15:35:53 +0200

    pradzia

---
+++ main.js
@@ -0,0 +1,22 @@
+// function arrayPlusArray(arr1, arr2) {
+//     let sum1 = 0;
+//     for (let i = 0, i < arr1.lenght, i++);
+//     sum1 += arr1[i];
+//     let sum2 = 0;
+//     for (let i = 0, i < arr1.lenght, i++);
+//     sum1 += arr2[i];
+    
+//     return arr1 + arr2; //something went wrong
+//   }
+
+
+
+//     console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+
+
+function(sumarray) {
+    
+}
+
+
+    console.log([1, 2, 3]);
